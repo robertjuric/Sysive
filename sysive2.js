3c445b93da706ce1454471c5f16787f634733403
Robert Juric <robert.juric@gmail.com>
2012-05-27 21:37:51 -0500

    First upload

---
+++ sysive2.js
@@ -0,0 +1,13 @@
+var Buffer = require('buffer').Buffer;
+var dgram = require('dgram');
+var log = require('util').log;
+
+sock = dgram.createSocket("udp4", function (msg, rinfo) {
+	log('got message from '+ rinfo.address +':'+rinfo.port);
+	log('data len: '+ rinfo.size + " data: "+ msg.toString('ascii', 0, rinfo.size));
+});
+
+sock.on('listening', function() {
+	log('Bound to 192.168.1.30:514');
+});
+sock.bind(514, '192.168.1.30');
